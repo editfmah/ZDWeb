83863598f94ea2351cf27f9e9175007e2b2627fc
Adrian Herridge <adrian@uncia.co.uk>
2024-05-16 21:38:03 +0100

    fix error with image properties

--- Sources/ZDWeb/WebUI/properties/Properties+Image.swift
+++ Sources/ZDWeb/WebUI/properties/Properties+Image.swift
@@ -8,18 +8,22 @@
 import Foundation
 
 public extension ImageProperties {
+    @discardableResult
     func src(_ src: String) -> Self {
         executingWebThread?.builderScript("\(builderId).src = '\(src)';")
         return self
     }
+    @discardableResult
     func alt(_ alt: String) -> Self {
         executingWebThread?.builderScript("\(builderId).alt = '\(alt)';")
         return self
     }
+    @discardableResult
     func scaleToFit() -> Self {
         executingWebThread?.builderScript("\(builderId).style.objectFit = 'contain';")
         return self
     }
+    @discardableResult
     func scaleToFill() -> Self {
         executingWebThread?.builderScript("\(builderId).style.objectFit = 'cover';")
         return self
